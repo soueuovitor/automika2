4e3019fadc55a9ca8663dd994505bb9fd10c8c7c
naishes <32779815+naishes@users.noreply.github.com>
2018-02-20 03:00:43 +0000

    jhhv

---
+++ controllers/main_veiculos.route.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const model = require('../models/veiculos.model');
+
+router.get('/', function(request, response){
+	//console.log(request.isAuthenticated());
+	model.list(function (veiculos) {
+
+	response.set("Content-Type", "text/html");
+	response.render('./front-end/main_veiculos', {
+		data: veiculos
+	})
+})
+
+});
+
+
+module.exports = router;

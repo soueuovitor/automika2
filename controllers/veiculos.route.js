ecd422572fafe86bd09ef820b3d8fdaeb2f82905
vitormiegsi <vhoa1995@hotmail.com>
2018-02-20 02:17:53 +0000

    crap

--- controllers/veiculos.route.js
+++ controllers/veiculos.route.js
@@ -1,27 +1,37 @@
 const express = require('express');
 const router = express.Router();
 const model = require('../models/veiculos.model');
-const fs = require ('fs');
+const fs = require('fs');
 const formidable = require('formidable');
 const resizeImg = require('resize-img');
 var Jimp = require("jimp");
 
 
-router.get('/', function(request, response){
+router.get('/', function (request, response) {
 	//console.log(request.user);
 	//console.log(request.isAuthenticated());
 	model.list(function (veiculos) {
-	response.set("Content-Type", "text/html");
-	response.render('./veiculos-list', {
-		data : veiculos
+		response.set("Content-Type", "text/html");
+		response.render('./veiculos-list', {
+			data: veiculos
+		})
 	})
-})
+});
+router.get('/:marca', function (request, response) {
+	//console.log(request.user);
+	//console.log(request.isAuthenticated());
+	model.filtraMarca(request.params.marca, function (veiculos) {
+
+		response.set("Content-Type", "text/html");
+		response.render('./veiculos-list', {
+			data: veiculos
+		})
+	})
 });
 
 
 
-
-router.get('/create', function(request, response) {
+router.get('/create', function (request, response) {
 	response.set("Content-Type", "text/html");
 	response.render('veiculos-item', {
 		isNew: true,
@@ -30,152 +40,161 @@ router.get('/create', function(request, response) {
 	})
 });
 
+router.post('/updateMarca/:marca', function (request, response) {
+	model.filtraMarca(request.params.marca, function (veiculos) {
+
+		response.set("Content-Type", "text/html");
+		response.render('veiculos-list', {
+			data: veiculos
+		})
+	})
+
+})
 
 
+router.post('/update/:matricula', function (request, response) {
 
-router.post('/update/:matricula', function(request, response) {
 
-	
 	var data = {
 
-		
-		'ano' : request.body.ano,
 
-		'km' : request.body.km,
+		'ano': request.body.ano,
 
-		'matricula' :request.params.matricula ,
+		'km': request.body.km,
 
-		'marca' : request.body.marca,
+		'matricula': request.params.matricula,
 
-		'modelo' : request.body.modelo,
+		'marca': request.body.marca,
 
-		'cilindrada' : request.body.cilindrada, 
+		'modelo': request.body.modelo,
+
+		'cilindrada': request.body.cilindrada,
 
 		'valor_compra': request.body.valor_compra,
 
-		'valor_venda' : request.body.valor_venda,
+		'valor_venda': request.body.valor_venda,
 
 		'despesas': request.body.despesas,
 
-		'cv' : request.body.cv		
-		
+		'cv': request.body.cv
+
 	};
 
 
-		model.update(data, function () {
-			
-			response.redirect('/veiculos')
-			
-	
-	})	
+	model.update(data, function () {
+
+		response.redirect('/veiculos')
+
+
+	})
 });
 
 
-router.get('/:username', function(request, response) {
-	model.read(request.params.username, function(user) {
+router.get('/:username', function (request, response) {
+	model.read(request.params.username, function (user) {
 		if (user != undefined) {
 			response.set("Content-Type", "text/html");
 			response.render('veiculos-item', {
 				isNew: false,
 				veiculos: user,
 				errors: []
-			})		
-		}else{
+			})
+		} else {
 			response.status(404).end();
 		}
-	})	
+	})
 });
 
 
 
-router.post('/create'  ,function (request, response) {
+router.post('/create', function (request, response) {
 
 	var form = new formidable.IncomingForm();
 	form.multiples = true;
 	var fields = request.fields;
-    form.parse(request, function (err, fields, files) {
-	var i = 0;
-	var paths = [];
+	form.parse(request, function (err, fields, files) {
+		var i = 0;
+		var paths = [];
 
-	
-	var num_fotos = files.logo.length;
-		for( var c of files.logo){
-			
+
+		var num_fotos = files.logo.length;
+		for (var c of files.logo) {
+
 			var oldpath = c.path;
 
-			var newpath = './public/img/' + fields.chassi  +'-'+ i +'.png';
-		
-		pics(oldpath,newpath);
-	  i++
+			var newpath = './public/img/' + fields.chassi + '-' + i + '.png';
+
+			pics(oldpath, newpath);
+			i++
 
 
 
-		  
+
 		}
 
-	
 
-		function pics(oldpath, newpath){
+
+		function pics(oldpath, newpath) {
 
 
 			Jimp.read(oldpath, function (err, lenna) {
-		
 
-				
+
+
 				if (err) throw err;
-				lenna.resize(1024, 768)            // resize 
-					 .quality(100)               
-					               
-					 .write(newpath); // save 
+				lenna.resize(1024, 768) // resize 
+					.quality(100)
+
+					.write(newpath); // save 
 			});
-			
+
 
 
 
 
 		}
 
-	
+
 		var data = {
-			
-			'matricula':fields.matricula,
-		
+
+			'matricula': fields.matricula,
+
 			'chassi': fields.chassi,
-			
-			'ano' : fields.ano,
 
-			'km' : fields.km,
+			'ano': fields.ano,
 
-			'marca' : fields.marca,
+			'km': fields.km,
 
-			'modelo' : fields.modelo,
+			'marca': fields.marca,
 
-			'cilindrada' : fields.cilindrada, 
+			'modelo': fields.modelo,
+
+			'cilindrada': fields.cilindrada,
 
 			'valor_compra': fields.valor_compra,
 
-			'valor_venda' : fields.valor_venda,
+			'valor_venda': fields.valor_venda,
 
 			'despesas': fields.despesas,
 
-			'cv' : fields.cv,
-			
-			'num_fotos' : num_fotos
+			'cv': fields.cv,
+
+			'num_fotos': num_fotos
 		};
 		model.create(data, function () {
-			
-			
+
+
 		});
 		response.redirect('/veiculos')
 
-							
-			
-			
-	
 
 
 
-});
+
+
+
+
+	});
 
 
 });

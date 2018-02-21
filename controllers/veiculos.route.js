5ba3dd64cf0945d2a6848e427a0184548a3b954f
Tiagoacb <33163740+Tiagoacb@users.noreply.github.com>
2018-02-21 20:26:50 +0000

    cxx

--- controllers/veiculos.route.js
+++ controllers/veiculos.route.js
@@ -12,7 +12,7 @@ router.get('/', function (request, response) {
 	//console.log(request.isAuthenticated());
 	model.list(function (veiculos) {
 		response.set("Content-Type", "text/html");
-		response.render('./veiculos-list', {
+		response.render('veiculos-list', {
 			data: veiculos
 		})
 	})
